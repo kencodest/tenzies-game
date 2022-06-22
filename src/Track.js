4c5666e3d214945bf4d68562754bdff4d3c41774
kencodest <muiaken88@gmail.com>
2022-06-22 14:10:12 +0300

    edit score tracker

--- src/Track.js
+++ src/Track.js
@@ -4,7 +4,7 @@ function Track(props) {
   return (
     <div className="records">
         <div className="recordsinfo">
-            <div className="bold">High Score:</div>
+            <div className="bold">Least Rolls:</div>
             <div>{props.score ? props.score : 0 }</div>
         </div>
         <div className="recordsinfo">
